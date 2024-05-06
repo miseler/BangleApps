11ef8e5001244fa02a2df5dee75b16345f15c64d
Alexander Miseler <github@miseler.de>
2024-05-06 11:40:17 +0200

    Update app.js

--- apps/infoclk/app.js
+++ apps/infoclk/app.js
@@ -356,8 +356,8 @@
         g.clearRect(0, SECONDS_TOP, g.getWidth(), SECONDS_TOP + DIGIT_HEIGHT)
           //.setFont('Vector', getFontSize(string.length, g.getWidth(), 6, DATE_LETTER_HEIGHT))
           //.setFont('6x12:2')
-          //.setFont6x15(2)
-          .setFont('6x15:3')
+          .setFont6x15(3)
+          //.setFont('6x15:3')
           .drawString(string, g.getWidth() / 2, DOW_DATE_CENTER_Y);
       }
 
