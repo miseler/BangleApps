4419b65b99c5e5db4be83d63451a19cb0178eded
Alexander Miseler <github@miseler.de>
2024-05-06 12:42:25 +0200

    Update app.js

--- apps/infoclk/app.js
+++ apps/infoclk/app.js
@@ -355,7 +355,7 @@
         let string = getWeatherString();
         g.clearRect(0, SECONDS_TOP, g.getWidth(), SECONDS_TOP + DIGIT_HEIGHT)
           //.setFont('Vector', getFontSize(string.length, g.getWidth(), 6, DATE_LETTER_HEIGHT))
-          .setFont('6x8:3')
+          .setFont('6x8:2x3')
           //.setFont6x15(2)
           //.setFont('HaxorNarrow7x17:1x2')
           .drawString(string, g.getWidth() / 2, DOW_DATE_CENTER_Y);
