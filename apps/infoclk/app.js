2f9b871dd5be818e1e963abd8e42da767c0b8095
Alexander Miseler <github@miseler.de>
2024-05-06 16:57:21 +0200

    Update app.js

--- apps/infoclk/app.js
+++ apps/infoclk/app.js
@@ -1,6 +1,7 @@
 {
   const FONT = require('infoclk-font.js');
   //require("Font8x12").add(Graphics);
+  require("FontDylex7x13").add(Graphics);
   
   const storage = require("Storage");
   const locale = require("locale");
@@ -351,11 +352,11 @@
       // Otherwise, use the seconds area
       else {
         //let string = getDayString(now) + ' ' + getDateString(now);
-        require("FontDylex7x13").add(Graphics);
+        
         let string = getWeatherString();
         g.clearRect(0, SECONDS_TOP, g.getWidth(), SECONDS_TOP + DIGIT_HEIGHT)
           //.setFont('Vector', getFontSize(string.length, g.getWidth(), 6, DATE_LETTER_HEIGHT))
-          .setFont("FontDylex7x13:1x2")
+          .setFont("Dylex7x13:1x2")
           //.setFont6x15(2)
           //.setFont('HaxorNarrow7x17:1x2')
           .drawString(string, g.getWidth() / 2, DOW_DATE_CENTER_Y);
