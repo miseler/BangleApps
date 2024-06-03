a4c8813da49159d56a4dbbfff910d8bfcf882b89
Alexander Miseler <github@miseler.de>
2024-06-03 21:00:59 +0200

    Update wid.js

--- apps/widdevst/wid.js
+++ apps/widdevst/wid.js
@@ -1,15 +1,15 @@
 (() => {
-  var stat = {date: 0};
-  var d = Date.now();
-  var settings = require("Storage").readJSON("widdevst.settings.json", 1)||{};
-  var redrawBars = "redrawBars" in settings ? settings.redrawBars : false;
+  let stat = {date: 0};
+  let d = Date.now();
+  let settings = require("Storage").readJSON("widdevst.settings.json", 1)||{};
+  let redrawBars = "redrawBars" in settings ? settings.redrawBars : false;
   delete settings;
 
   WIDGETS.devst = {area: "tr", width: 22, draw: function() {
     d = Date.now();
     if (WIDGETS.devst._draw) return;
     /*
-    var t;
+    let t;
     if ((d - stat.date) < 6e4) {
       t = process.memory(false);
     } else {
@@ -20,14 +20,13 @@
     }
     t = t.usage / t.total;
     */
-    var x = this.x;
-    var y = this.y;
+    let x = this.x;
+    let y = this.y;
     g.reset();
-    // Render inside the box this.x, this.y, this.x + this.width-1, this.y + 23
     g.clearRect(x, y, x + 21, y + 23);
     g.setFont('6x8', 1);
-    var again = false;
-    var toggleForce = true;
+    let again = false;
+    let toggleForce = true;
     if (NRF.getSecurityStatus().connected || toggleForce) g.drawString('B', x + 5, y + 3), again = true;
     if (Bangle.isCompassOn() || toggleForce) g.drawString('C', x + 13, y + 3), again = true;
     if (Bangle.isGPSOn() || toggleForce) g.drawString('G', x + 5, y + 12), again = true;
@@ -37,15 +36,17 @@
     /*
     g.drawRect(x + 2, y + 1, x + 20, y + 21);
     /*/
-    var b = E.getBattery();
-    var corner = 4;
+    let b = E.getBattery();
+    let corner = 4;
     if(b <= 65) {g.drawLine(x+21-corner, y+0, x+21, y+0);g.drawLine(x+21, y+1, x+21, y+corner);}
+    if(b <= 40) {g.drawLine(x+21-corner, y+22, x+21, y+22);g.drawLine(x+21, y+22-corner, x+21, y+21);}
+    if(b <= 15) {g.drawLine(x+corner, y+22, x+0, y+22);g.drawLine(x+0, y+22-corner, x+0, y+21);}
     //b=100;
     setColor(b);
-    var top = E.clip(b-75, 0, 25);
-    var right = E.clip(b-50, 0, 25);
-    var bottom = E.clip(b-25, 0, 25);
-    var left = E.clip(b, 0, 25);
+    let top = E.clip(b-75, 0, 25);
+    let right = E.clip(b-50, 0, 25);
+    let bottom = E.clip(b-25, 0, 25);
+    let left = E.clip(b, 0, 25);
     if(top>0) g.drawRect(x+1,y+0,x+1+20*top/25,y+1);
     if(right>0) g.drawRect(x+20,y+0,x+21,y+22*right/25);
     if(bottom>0) g.drawRect(x+1,y+21,x+1+20*bottom/25,y+22);
@@ -66,10 +67,10 @@
     else g.setColor(b > 50 ? '#0f0' : (b > 25 ? '#f80' : '#f00'));
   }
 
-  var draw = WIDGETS.devst.draw.bind(WIDGETS.devst);
+  let draw = WIDGETS.devst.draw.bind(WIDGETS.devst);
 
-  var drawTime = () => Bangle.isLocked() ? 6e4 : 2e3;    // TODO: human readable?
-  var throttledDraw = () => Date.now() - d > drawTime() && draw();
+  let drawTime = () => Bangle.isLocked() ? 6e4 : 2e3;    // TODO: human readable?
+  let throttledDraw = () => Date.now() - d > drawTime() && draw();
 
   Bangle.on("HRM", throttledDraw);
   Bangle.on("GPS", throttledDraw);
