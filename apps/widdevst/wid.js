a8a1f77153143bfe0e32df1f3c5d038c2e9d6748
Alexander Miseler <github@miseler.de>
2024-06-03 17:03:05 +0200

    Update wid.js

--- apps/widdevst/wid.js
+++ apps/widdevst/wid.js
@@ -8,6 +8,7 @@
   WIDGETS.devst = {area: "tr", width: 22, draw: function() {
     d = Date.now();
     if (WIDGETS.devst._draw) return;
+    /*
     var t;
     if ((d - stat.date) < 6e4) {
       t = process.memory(false);
@@ -18,19 +19,27 @@
       t = process.memory();
     }
     t = t.usage / t.total;
+    */
+    var b = E.getBattery();
     var x = this.x;
     var y = this.y;
     g.reset();
     g.clearRect(x, y, x + 21, y + 23);
-    g.drawRect(x + 2, y + 1, x + 20, y + 21);
+    //g.drawRect(x + 2, y + 1, x + 20, y + 21);
+    if(b>=75) g.drawRect(x+2,y+1,x+20,y+2);
+    if(b>=50) g.drawRect(x+19,y+1,x+20,y+21);
+    if(b>=25) g.drawRect(x+2,y+20,x+20,y+21);
+    g.drawRect(x+2,y+1,x+3,y+21);
+    // TODO: why rect and not line?
     g.setFont('6x8', 1);
     var again = false;
-    if (NRF.getSecurityStatus().connected) g.drawString('B', x + 5, y + 3), again = true;
-    if (Bangle.isCompassOn()) g.drawString('C', x + 13, y + 3), again = true;
-    if (Bangle.isGPSOn()) g.drawString('G', x + 5, y + 12), again = true;
-    if (Bangle.isHRMOn()) g.drawString('H', x + 13, y + 12), again = true;
-    g.setColor(col(stat.sto)); g.drawRect(x + 2, y + 21, x + 2 + stat.sto * 18, y + 22);
-    g.setColor(col(t)); g.drawRect(x + 1, y + 21 - t * 20, x + 2, y + 21);
+    var toggleForce = true;
+    if (NRF.getSecurityStatus().connected || toggleForce) g.drawString('B', x + 5, y + 3), again = true;
+    if (Bangle.isCompassOn() || toggleForce) g.drawString('C', x + 13, y + 3), again = true;
+    if (Bangle.isGPSOn() || toggleForce) g.drawString('G', x + 5, y + 12), again = true;
+    if (Bangle.isHRMOn() || toggleForce) g.drawString('H', x + 13, y + 12), again = true;
+    //g.setColor(col(stat.sto)); g.drawRect(x + 2, y + 21, x + 2 + stat.sto * 18, y + 22);
+    //g.setColor(col(t)); g.drawRect(x + 1, y + 21 - t * 20, x + 2, y + 21);
     // if there's nothing active, don't queue a redraw (rely on Bangle.on(...) below)
     if (redrawBars || again) setTimeout(draw, drawTime());
   }};
@@ -41,7 +50,7 @@
 
   var draw = WIDGETS.devst.draw.bind(WIDGETS.devst);
 
-  var drawTime = () => Bangle.isLocked() ? 6e4 : 2e3;
+  var drawTime = () => Bangle.isLocked() ? 6e4 : 2e3;    // TODO: human readable?
   var throttledDraw = () => Date.now() - d > drawTime() && draw();
 
   Bangle.on("HRM", throttledDraw);
